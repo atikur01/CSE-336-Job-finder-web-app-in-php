536499b47d6938f516e5f4f5aec80da37718a1fa
Atikur <coc13259@gmail.com>
2023-05-15 17:08:23 +0600

    1

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ $array = array();
     href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap"
     rel="stylesheet">
 
-  <title>Job finder</title>
+  <title>Blog</title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
