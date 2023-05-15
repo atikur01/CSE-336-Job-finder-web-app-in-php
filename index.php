56fd20824376640394c295bcec1cafb68222de28
Atikur <coc13259@gmail.com>
2023-05-15 17:09:14 +0600

    1

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ https://templatemo.com/tm-551-stand-blog
     <nav class="navbar navbar-expand-lg">
       <div class="container">
         <a class="navbar-brand" href="index.php">
-          <h2>Job Finder<em>.</em></h2>
+          <h2>Blog<em>.</em></h2>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
           aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
